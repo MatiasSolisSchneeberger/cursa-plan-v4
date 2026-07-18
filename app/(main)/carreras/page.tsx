9a5a9b72b84598e22b9bb421079960317983023d
Matias Solis Schneeberger <matias.solis.sch@gmail.com>
2026-07-18 00:40:54 -0300

    creando pagina de carrera

---
+++ app/(main)/carreras/page.tsx
@@ -0,0 +1,68 @@
+import {Tabs, TabsContent, TabsList, TabsTrigger} from "@/components/ui/tabs"
+import {getCarrerasConPlanes} from "@/lib/carreras"
+import CardCarrera from "@/components/CardCarrera"
+
+export default async function CarreraPage() {
+	const carreras = await getCarrerasConPlanes()
+
+	const filtros = [
+		{
+			filtro: "todos",
+			label: "Ver todos",
+		},
+		{
+			filtro: "ingenieria",
+			label: "Ingenierías",
+		},
+		{
+			filtro: "licenciatura",
+			label: "Licenciaturas",
+		},
+		{
+			filtro: "profesorado",
+			label: "Profesorados",
+		},
+	]
+
+	return (
+		<section className="relative flex w-full shrink-0 flex-col flex-wrap content-start items-center justify-center gap-6 self-stretch">
+			<h2 className="typeset">Carreras</h2>
+
+			<Tabs defaultValue="todos" className="w-full">
+				<TabsList>
+					{filtros.map(({filtro, label}) => {
+						return (
+							<TabsTrigger key={label} value={filtro}>
+								{label}
+							</TabsTrigger>
+						)
+					})}
+				</TabsList>
+				{filtros.map(({filtro}) => {
+					return (
+						<TabsContent
+							key={filtro}
+							value={filtro}
+							className="relative grid w-full shrink-0 grid-cols-1 flex-wrap content-start items-start justify-start gap-6 self-stretch md:grid-cols-2 lg:grid-cols-3">
+							{carreras
+								.filter((carrera) => {
+									if (filtro === "todos") return true
+									return carrera.slug.includes(filtro)
+								})
+								.map(({icon, id, nombre, planes, slug}) => {
+									const formattedPlanes =
+										planes
+											?.map((p) => ({
+												anio: p.anio_inicio,
+												hasMaterias: p.materia_plan && p.materia_plan.length > 0,
+											}))
+											.sort((a, b) => b.anio - a.anio) || []
+									return <CardCarrera key={id} icon={icon} slug={slug} carrera={nombre} planes={formattedPlanes} />
+								})}
+						</TabsContent>
+					)
+				})}
+			</Tabs>
+		</section>
+	)
+}
