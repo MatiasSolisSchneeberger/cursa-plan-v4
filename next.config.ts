c59d38c14c7603ef2718a0080256e0a839b4ca97
Matias Solis Schneeberger <matias.solis.sch@gmail.com>
2026-08-05 13:15:35 -0300

    actualizando package y config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type {NextConfig} from "next"
+import createMDX from '@next/mdx'
 
 const nextConfig: NextConfig = {
 	cacheComponents: true,
+	pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
 }
 
-export default nextConfig
+const withMDX = createMDX({
+	extension: /\.(md|mdx)$/,
+})
+
+export default withMDX(nextConfig)
