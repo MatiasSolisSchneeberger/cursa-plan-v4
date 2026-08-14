07550e3b4eed63f9d5753ab70e063ff93ee0192b
Matias Solis Schneeberger <matias.solis.sch@gmail.com>
2026-08-14 11:34:27 -0300

    arreglando altura del contendido en /(main)

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -3,7 +3,7 @@ import Footer from "@/sections/Footer"
 
 export default function MainLayout({children}: {children: React.ReactNode}) {
 	return (
-		<main className="px-4 py-2 sm:px-4 md:px-8 lg:px-12 flex flex-col gap-4 min-h-full w-full">
+		<main className="px-4 py-2 sm:px-4 md:px-8 lg:px-12 flex flex-1 flex-col gap-4 w-full">
 			<Navbar />
 			<div className="flex-1">{children}</div>
 			<Footer />
